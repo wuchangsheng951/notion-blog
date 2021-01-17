5058b998f840189ba97d7ca480633c6a9bd96e6e
wuchangsheng951 <sunhuiming55@gmail.com>
2021-01-16 19:40:13 -0500

    first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default () => (
         width="250"
         alt="Vercel + Notion"
       />
-      <h1>My Notion Blog</h1>
+      <h1>兰妈妈妈妈</h1>
       <h2>
         Blazing Fast Notion Blog with Next.js'{' '}
         <ExtLink
